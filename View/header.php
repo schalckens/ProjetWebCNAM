5b539fc3986dfad227155f69e34fa508766cb9c7
schalckens <v.schalckens@gmail.com>
2024-03-18 11:19:45 +0100

    retravail visuel header/footer/connexion

--- View/header.php
+++ View/header.php
@@ -1,22 +1,37 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Site de Jeux de Films</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Lien vers la feuille de style CSS -->
     <link rel="stylesheet" href="styles/style.css">
 </head>
+
 <body>
-<header>
-    <nav>
-        <ul>
-            <li><a href="/accueil">Accueil</a></li>
-                <li><a href="/login">Connexion</a></li>
-                <li><a href="/register">Inscription</a></li>
-                <li><a href="/game">Jeu</a></li>
-                <li><a href="/disconnect">Déconnexion</a></li>
-        </ul>
-    </nav>
-</header>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container">
+                <a class="navbar-brand" href="/accueil">Nom App</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse collapse" id="navbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a href="/login">Connexion</a></li>
+                        <li class="nav-item"><a aria-disabled="true">Déconnexion</a></li>
+                    </ul>
+                    <!-- <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="/login">Connexion</a>
+                        <a class="nav-link" href="/disconnect">Déconnexion</a>
+                        <a class="nav-link disabled" aria-disabled="true">Déconnexion</a>
+                    </div> -->
+                </div>
+            </div>
+        </nav>
+    </header>
