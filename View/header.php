4d27a6db3ecdba2ec7b44890485c00602b8ee2ff
schalckens <v.schalckens@gmail.com>
2024-03-18 13:56:17 +0100

    Update header.php

--- View/header.php
+++ View/header.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Site de Jeux de Films</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Lien vers la feuille de style CSS -->
     <link rel="stylesheet" href="styles/style.css">
 </head>
@@ -14,33 +15,35 @@
 <body>
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false"
-                    aria-label="Toggle navigation">
+            <div class="container-fluid">
+                <button 
+                    class="navbar-toggler" 
+                    type="button" 
+                    data-toggle="collapse"
+                    data-target="#navbar1" 
+                    aria-controls="navbar1" 
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="/accueil">Nom App</a>
                 
-                <div class="navbar-collapse collapse" id="navbar">
-                    <ul class="navbar-nav">
+                
+                <div class="collapse navbar-collapse" id="navbar1">
+                    <a class="navbar-brand" href="/accueil">  <span class="fa fa-home"></span> Nom App</a>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <?php if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true): ?>
-                        <li class="nav-item"><a class="nav-link" href="/login">Connexion</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/register">Inscription</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/login">Connexion</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/register">Inscription</a></li>
                     <?php elseif (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] !== true): ?>
-                        <li class="nav-item"><a class="nav-link" href="/game">Jeu</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/disconnect">Déconnexion</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/game">Jeu</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/disconnect">Déconnexion</a></li>
                     <?php else: ?>
-                        <li class="nav-item"><a class="nav-link" href="/backoffice">Back Office</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/game">Jeu</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/disconnect">Déconnexion</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/backoffice">Back Office</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/game">Jeu</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/disconnect">Déconnexion</a></li>
                     <?php endif; ?>
                     </ul>
-                    <!-- <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="/login">Connexion</a>
-                        <a class="nav-link" href="/disconnect">Déconnexion</a>
-                        <a class="nav-link disabled" aria-disabled="true">Déconnexion</a>
-                    </div> -->
                 </div>
             </div>
         </nav>
