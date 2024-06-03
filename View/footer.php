a193479b850b02be6a92e3e7c004fbd0a882f9cf
schalckens <v.schalckens@gmail.com>
2024-06-03 14:28:57 +0200

    w3c checker accueil OK

--- View/footer.php
+++ View/footer.php
@@ -1,9 +1,8 @@
-</body>
 <footer>
     <div class="container"  data-bs-theme="dark">
         <hr>
         <p class="text-center" id="copyright">&copy; <?= date("Y") ?> Detective du Cinéma un projet de Darmoise Zac, Kerebel Bastien et Schalckens Valentine.</p>
-    <div>
+    </div>
 </footer>
 </body>
 </html>
